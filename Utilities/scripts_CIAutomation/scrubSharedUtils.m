e96c8845afd1da099d9ee1965273395349720dda
jkhoury <jkhoury@MATHWORKS-BLLzo>
2026-01-13 14:21:26 +0000

    Removed CI utility files

--- Utilities/scripts_CIAutomation/scrubSharedUtils.m
+++
@@ -1,7 +0,0 @@
-function scrubSharedUtils()
-% Clear shared utils from previous builds for clean Codegen/SiL
-    prj = currentProject;
-    fldrname = fullfile(prj.SimulinkCodeGenFolder,'slprj');
-    if isfolder(fldrname)
-        rmdir(fldrname,'s')
-    end 
